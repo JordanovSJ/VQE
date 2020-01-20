7085ab384b6c6a9039223a216181ea47e45797db
Jordan Jordanov <jordanovsj@gmail.com>
2020-01-20 20:37:07 +0000

    commit backends before merge to master

--- src/__main__.py
+++ src/__main__.py
@@ -7,13 +7,14 @@ import time
 
 if __name__ == "__main__":
 
-    molecule = H2
+    molecule = HF
     max_n_iterations = 500
 
-    excitation_list = UCCSD(molecule.n_orbitals, molecule.n_electrons).get_excitation_list()
+    excitation_list = UCCSD(molecule.n_orbitals, molecule.n_electrons).get_excitation_list()[:15]
+    excitation_list += UCCSD(molecule.n_orbitals, molecule.n_electrons).get_excitation_list()[-10:]
 
     # backend=QiskitSimulation, # not working
-    vqe_runner = VQERunner(molecule, backend=QiskitSimulation, excitation_list=excitation_list, molecule_geometry_params={'distance': 0.735}) #,excitation_list=UCCGSD(molecule.n_orbitals, molecule.n_electrons).get_excitation_list())
+    vqe_runner = VQERunner(molecule, backend=QiskitSimulation, excitation_list=excitation_list, molecule_geometry_params={'distance': 0.917}) #,excitation_list=UCCGSD(molecule.n_orbitals, molecule.n_electrons).get_excitation_list())
 
     t0 = time.time()
     result = vqe_runner.vqe_run(max_n_iterations)
