e11a11ff2529b32eb0c05484158625215a6ba8e5
jordan <jordanovsj@gmail.com>
2020-02-21 17:35:26 +0000

    test double exchange ansatz

--- src/__main__.py
+++ src/__main__.py
@@ -22,10 +22,12 @@ if __name__ == "__main__":
 
     # uccsd = UCCSD(molecule.n_orbitals, molecule.n_electrons)
     ansatz_element_1 = DoubleExchangeAnsatzElement([4, 5], [10, 11])
-    ansatz_element_2 = DoubleExchangeAnsatzElement([2, 3], [10, 11])
+    ansatz_element_2 = DoubleExchangeAnsatzElement([2, 3], [10, 11]) #.59660
+    ansatz_element_3 = DoubleExchangeAnsatzElement([8, 9], [10, 11])
+
     # ansatz_element_1 = DoubleExchangeAnsatzElement([0, 1], [2, 3])
     # ansatz_element_1 = ExchangeAnsatz1(molecule.n_orbitals, molecule.n_electrons, n_blocks=2)
-    ansatz_elements = [ansatz_element_1, ansatz_element_2]
+    ansatz_elements = [ansatz_element_1, ansatz_element_2, ansatz_element_3]
 
     vqe_runner = VQERunner(molecule, backend=QiskitSimulation, ansatz_elements=ansatz_elements,
                            molecule_geometry_params={'distance': r}, print_var_parameters=True)#, optimizer='Nelder-Mead')
