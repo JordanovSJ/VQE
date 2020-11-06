e0077e48896f35f9e9c7108259da0f93faa4e83b
Jordan Jordanov <jordanovsj@gmail.com>
2020-11-06 15:16:53 +0000

    added sf and df excitations to loading fro csv

--- scripts/energy_for_specific_conf.py
+++ scripts/energy_for_specific_conf.py
@@ -22,13 +22,13 @@ import ast
 
 if __name__ == "__main__":
 
-    r = 1
-    molecule = LiH(r=r)  #frozen_els={'occupied': [0, 1], 'unoccupied': []})
+    r = 1.316
+    molecule = BeH2(r=r)  #frozen_els={'occupied': [0, 1], 'unoccupied': []})
 
     # logging
     LogUtils.log_config()
 
-    df = pandas.read_csv("../results/iter_vqe_results/vip/LiH_h_adapt_gsdqe_comp_pairs_r=1_24-Sep-2020.csv")
+    df = pandas.read_csv("../results/iter_vqe_results/vip/BeH2_g_adapt_gsdfe_27-Aug-2020.csv")
     # df = pandas.read_csv("../x_sdfsd.csv")
 
     state = DataUtils.ansatz_from_data_frame(df, molecule)
@@ -46,10 +46,12 @@ if __name__ == "__main__":
     optimizer = 'BFGS'
     optimizer_options = {'gtol': 1e-8}
 
-    vqe_runner = VQERunner(molecule, backend=QiskitSim, optimizer=optimizer, optimizer_options=None,
-                           print_var_parameters=False, use_ansatz_gradient=True)
+    # vqe_runner = VQERunner(molecule, backend=QiskitSim, optimizer=optimizer, optimizer_options=None,
+    #                        print_var_parameters=False, use_ansatz_gradient=True)
+    #
+    # energy = vqe_runner.vqe_run(ansatz=ansatz, init_guess_parameters=var_parameters,
+    #                             init_state_qasm=None, cache=global_cache)
 
-    energy = vqe_runner.vqe_run(ansatz=ansatz, init_guess_parameters=var_parameters,
-                                init_state_qasm=None, cache=global_cache)
+    ansatz_grad = QiskitSim.ansatz_gradient(var_parameters, ansatz, molecule, cache=global_cache)
 
-    print(energy)
+    print(ansatz_grad)
