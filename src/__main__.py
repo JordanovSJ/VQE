94bc151359a7b71057c543714973f7874af8384a
jordan <jordanovsj@gmail.com>
2020-11-17 10:46:37 +0000

    fix main

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,6 @@
 from src.vqe_runner import VQERunner
 from src.q_systems import *
-from src.ansatze import *
+from src.ansatz_element_sets import *
 from src.backends import QiskitSim
 from src.utils import LogUtils
 
