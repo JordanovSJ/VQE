fc280ecb9350a3eb968e5e979aaedde58adaa8f2
apprenticeadi <zhenghao.li.1999@gmail.com>
2021-02-14 15:59:15 +0000

    Test different n_shots

--- scripts/zhenghao/qasm_backend_test.py
+++ scripts/zhenghao/qasm_backend_test.py
@@ -1,3 +1,4 @@
+import pandas
 
 from src.q_systems import LiH, H2, H4
 from scripts.zhenghao.noisy_backends import QasmBackend
@@ -38,15 +39,31 @@ message = 'QiskitSimBackend result is {}'.format(expectation_value_0)
 logging.info(message)
 
 # <<<<<<<<<< QasmBackend >>>>>>>>>>>>.
-time_0 = time.time()
-expectation_value_1 = QasmBackend.ham_expectation_value(var_pars, ansatz, molecule)
-time_1 = time.time()
-time_total = time_1 - time_0
+n_shots_list = [1, 5, 10, 50, 100, 200, 500]
+n_it = len(n_shots_list)
+exp_val_list = np.zeros(n_it)
+time_list = np.zeros(n_it)
 
-message = 'QasmBackend result is {}'.format(expectation_value_1)
+i=0
+for n_shots in n_shots_list:
+    time_0 = time.time()
+    expectation_value_1 = QasmBackend.ham_expectation_value(var_pars, ansatz, molecule, n_shots=n_shots)
+    time_1 = time.time()
+    time_total = time_1 - time_0
 
-logging.info(message)
+    message = 'QasmBackend result is {} for n_shots={}'.format(expectation_value_1, n_shots)
 
-message_time = 'time used = {}s'.format(time_total)
+    logging.info(message)
 
-logging.info(message_time)
+    message_time = 'time used = {}s'.format(time_total)
+
+    logging.info(message_time)
+
+    exp_val_list[i] = expectation_value_1
+    time_list[i] = time_total
+
+
+data = {'n_shots': n_shots_list, 'expectation_value': exp_val_list, 'time': time_list}
+df = pandas.DataFrame(data)
+df.to_csv('csv_folder/{}_different_nshots_{}.csv'.format(molecule.name, time_stamp))
+
