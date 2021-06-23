eff18818ba10b4c81d31ba770763393624a84ac8
Jordan Jordanov <jordanovsj@gmail.com>
2021-06-23 20:19:39 +0100

    write data to csv file

--- electronic_systems/vqe_Sz.py
+++ electronic_systems/vqe_Sz.py
@@ -8,6 +8,7 @@ from src.backends import *
 from src.utils import LogUtils
 from src.cache import *
 from pylab import *
+import pandas
 
 
 def ansatz_1():
@@ -40,18 +41,25 @@ if __name__ == "__main__":
 
     S_values, U_values = [], []
 
+    count = 0
     for U in numpy.linspace(0.1,0.4,30):
+        print(count)
+        count += 1
         H = ham_14_qubits(U)
         e_system = ElectronicSystem(H, n_orbitals, n_electrons)
 
         ansatz = ansatz_1()
+
+        ## Uncomment this to change the spin to +1
+        # init_qasm = SFExc(1, 12).get_qasm(numpy.pi/2)
+
         init_qasm = None
 
         global_cache = GlobalCache(e_system, excited_state=0)
         global_cache.calculate_exc_gen_sparse_matrices_dict(ansatz)
 
         optimizer = 'BFGS'
-        optimizer_options = {'gtol': 10e-8, 'maxiter': 10}
+        optimizer_options = {'gtol': 10e-8}
         vqe_runner = VQERunner(e_system, backend=backend, print_var_parameters=False, use_ansatz_gradient=True,
                            optimizer=optimizer, optimizer_options=optimizer_options)
 
@@ -69,10 +77,26 @@ if __name__ == "__main__":
 
         S_values.append(expectation_value[0,0]/4)
         U_values.append(U)
-        del global_cache
+        # del global_cache
 
-    plt.plot(S_values, U_values, 'rx')
-    plt.show()
+        print(U_values)
+        print(S_values)
+
+        df = pandas.DataFrame(columns=['U', 'S'])
+        df['U'] = U_values
+        df['S'] = S_values
+        df.to_csv('S_vs_U.csv')
+
+    print(U_values)
+    print(S_values)
+
+    df = pandas.DataFrame(columns=['U', 'S'])
+    df['U'] = U_values
+    df['S'] = S_values
+    df.to_csv('S_vs_U.csv')
+
+    # plt.plot(S_values, U_values, 'rx')
+    # plt.show()
 
 
 
